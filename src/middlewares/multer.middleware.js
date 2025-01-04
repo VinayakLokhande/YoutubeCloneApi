7ab718b9110fa2adab6e2806435227fd9622c642
Vinayak Lokhande <vinayaklokhande1011@gmail.com>
2025-01-04 07:55:01 +0530

    almost all configuration work done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,27 @@
+import multer from "multer";
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function(req, file, cb) {
+        // const uniqueSffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.fieldname + '-' + uniqueSuffix)
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage: storage
+})
+
+
+/*
+72) so multer la shiknyasathi apnala doc vachavech lagnar multer che so multer cha github page ahe doc cha tithe jaun vachu shakto. so asa apan router lihito na so adi apan kasa lihaycho app.post('/profile', function(req, res, next)) asa na so ata yatach middleware apan takat asto route and callback function cha madhe like app.post('/profile', upload.single('avtar'), funtion(req, res, next)) asa. so madhe upload.single() he apan middlware taklay ata upload.single he multer cha ahe like mala photo upload karaychay ki profile route hit zala ki mala photo first upload karaychay asa ahe te basically. so single photo upload karaycha asel or multiple apnala middlware lagnarach. so he sagla mi je sangtoy single, multiple he functions so he sagle multer cha documentation page je ahe github cha tithe ahe so tithe jaun bagaych parallely. so zala upload but mi save kasa karnar na. so apnala tya file information section madhe options distat so apan local server la means aplya phone or computer madhe first file la save karaych mhanlo la upload karaycha adi. so tithe apnala destination, buffer he disel so destination means he aplya diskstorage madhe save karnyacha option ahe and buffer ha aplya memory madhe. so memory madhe jrr save kela trr jrr clean nahi zali tr ti bharu shakte na so tyamule disk madhe save karaych. so tithe doc madhe apnala DiskStorage kasa use karaych yach implementation pn dialy so ahe tasach apnala pn karaychay. 
+
+
+73) so multer la import kela. so destination function  req, file, cb ghetay na so cb means call back. so te file ahe na parameter te multer kadech asta. req trr aplyakade astech ji user kadun req aliye ti. ek file access ithe apnala ek anki milto jyacha mule saglya files cha access apnala milto. req cha at trr json data asto to apnala miltoch and jrr file pn yetiye so tyachasathich multer use hoto. bec req body madhe json data vagere apan configure trr kela express madhe but file nahi configure karu shakat. so yasathich multer or express cha file uploader use hoto. jenekarun mala madhe ek option milto jyacha through mi file upload karu shakto. now cb madhe first param trr null ahe which is error parameter so ata error la nahi handle karuyat so second param is destination. so means apan aple sagle files vagere je save karaychet apnala temporary to path ithe dyaycha. so mi thevnare same directory madhe so dot dila and then public and temp. so yachsathi baga apan ek folder create kelay na public/temp so to yachsathi kelay. so mi saglya files public folder madhe thevnar so mala easily tyacha access milel. then second option is filename. so file ji save keli na apan public folder madhe so ticha name apnala kasa thevaychay he aplyavar asta kahi pn theu shakto apan just unique asav. so tithe code bagu shakta varti comment kelay na uniqueSuffix ahe na so tasa apan file cha name generate karu shakto and then cb madhe tyala file name barobar attach kelay so file madhe pn barech options apnala miltat. like fieldName, fileName, destinationName, mimeType, then anki ek milta which is originalName means je user ne file cha name thevlela upload kartana tech apan access karu shakto. but originalName thevna kahi vela problem generate karu shakta. bec jrr user ne ekach name cha multiple files ekdam upload kelya trr files override hou shaktat na. but ithe aplya case madhe ti case khup kami ahe bec apan file cloudinary la upload zalya zalya lagech delete pn karnare na aplya local server varun so evdi need nahi but best practice hech ahe ki new unique name generate karun mg save karaycha. and then ya storage la export kela so ata apan ya upload variable la as a middlware controller lihitana madhe use karu shakto and easily file pass karu shakto. so zala he multer cha pn configuration zala. 
+
+74) so jenva apan app.post('/profile', function(req, res, next)) he lihito so tenva /profile yala route boltat and function() je ahe pudhe tyala controllers boltat. so jenva apan routes and controller lihit asu trr jrr ekada route apnala file provide karat asel trr to route lihitana madhe ya multer cha upload.storage karun mi yala call karnar tithe and tithe mg apan file che operations karu shakto so yalach middlware boltat.so jenva pn apan ya storage la call karu tenva mala filename pn miltay na so localpath vali ji apli gost hoti ti ithe solve houn jate bec multer.diskStorage je function ahe te fileName lach return karta. means jo pn localpath ala pahije to sagla apan gheu shakto. like bolo na varti file dot karun options access karu shakto tithe. so almost 95% ready zaloy aplya project cha configuration karun. so apan ataparyant bagitla trr ky kela trr just project setup kelay. like apan ajunparyant ek single controller nahi lihila, route nahi lihila, aplya app file madhe pn ajun jast kahi nahi. so asach asta companies madhe pn production grade code lihitana ha sagla setup karavach lagto. so apan ata kahi routes and controllers lihu shakto like registration che vagere, te routes postman var kase use karayche vagere sagla bagnare.
+*/
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import {v2 as cloudinary} from cloudinary
+import fs from fs
+
+cloudinary.config(
+    {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    }
+)
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("MY MESSAGE : file is uploaded successfully on cloudinary ", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+
+export {uploadOnCloudinary}
+
+
+
+/*
+68) now apan toparyant local server varti file la save karun thevlay na. so ajun tyach implementation nahi kela but suppose ki apan kelay so mg apan ky karnare tya local server madla file path denar cloudinary la ki ithli file ghe and save kar asa. and also nantrr file successfully upload zali ki tila local server varun remove pn karaychay. so cloudinary la import kela v2 karayla sangtay cloudinary so v2 la kela and tyacha name dila as cloudinary apan v2 lach use karu shakto but samjaysathi dila. then anki ek import kela which is fs so he fs ky ahe trr 'file system'. so node cha at apnala file system pn milta. so hila import nahi karav lagat apnala nodejs madhunach default apnala hi library milte. so google la nodejs fs takla ki yach documentation yeta so te vachaycha. so tyat apnala file la read, write, update etc. karayche functions deta or open and close deirectories karaych deta so file handling sathi je kahi lagta te sagla apnala he deta. so file handling madhe ek concept aste link and unlink. so jenva pn apan file la delete karat asto aplya suppose file explorer madhun tenva ti unlink hot aste. karan file system madhe delete, remove asa kahi nahi bola jat and ase functions pn apnala nahit milat tyalach ithe link and unlink bola jata. so asa apan file cloudinary var upload zali ki tila unlink karu shakto.
+
+69) so cloudinary la configure karav lagta so yacha doc madhe code mile to ghetla cloudinary.configure so te cloud_name, api_key, api_secret magta so he sagla apan cloudinary varunach gheu shakto he aplyasathi generate zalela asta. so ya keys, name apan .env file madhe thevnar so tithe CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY and CLOUDINARY_API_SECRET he ghetle and tyat tya keys anun thevlya. so ata ky karu shakto apan ki apan ek method banavaychi tya method cha parameter madhe apan apla local server cha file path denar and tyala mg nantrr upload karaych cloudinary var. so jrr upload zala ki tya file la unlink karaych. so file handling pn database sarkhach asta yat pn barech issues yeu shaktat so tyamulech try catch madhe lihaych and also yana time lagto so async await pn lavaych. so ek variable create kela uploadOnCloudinary and tyat arrow function lihila async laun and tyat ek param ghetla localFilePath jyach name apan kahi pn deu shakto. so he method kasa and kute use karaych he kalel apnala pudhe. so try catch lavla and try madhe condition lavli ki file path ahe ka nasel trr return kar. ithe apan error pn lau shakto ki file path nahi sapdla ashi. then next step karu file la upload karaych. so cloudinary madhe ek method milte apnala uploader and tyatun milte upload method so tyat ghetlya and tyat localfilepath la pass kela and also kahi ajun pn params deu shakto apan docs vachun bagu shakto so yat coma lavla trr tithe disel ki uploadOptions ek parameter ahe upload madhe so tyat milat resource_type so yat apan image or raw or video ase options specifically deu shakto ki maza file cha type ha ha asnare but ata mi auto ghetla so auto ne cloudinary automatic swataha file kutlay type chi ahe te detect kartat. so he time ghenar so await lavla and eka variable madhe store kela. then yala console log kela so yat apnala barich infor milte response madhe. so yala console kela so yatun apnala url milto ji file apan upload keli na so ti upload zalyanantrr apanala ek url mitlo so to print kela. and then user la saglacha sagla res return kela.
+
+70) now file jrr successfully upload zali nasel trr pn ek catch cah option apanala banto and jrr localfilepath detana kahi mistake zali asel trr tenva pn kahi tri option apanala handle karnyasathi banto. so jrr filepath chukla asel or kahi issue ala asel file upload kartana trr tya case madhe apnaala aplya local server var ji ti file temporary upload karun thevli hoti tila unlink pn kela pahije na bec ti file trr bina kamachi resources kahnar na tenva. so tyamule catch madhe fs je apan ghetla file system tyat unlink cha ek option milto and dusra unlinkSync ek means jovar unlink hot nahi filepath toparyant pudhe jau naka ithech synchronously thamba and tyat localFilePath la pass kela and yatun return kela null. and uploadOnCloudinary la export kela. so zala asa apan upload karto file la cloudinary var. so ata video aso , pdf aso, image aso or kutlya pn type chi file aso apan tila asach upload karu shakto.
+
+71) now mala ek middleware banavaychay. so ata apan multer cha use karun middleware banavnare. so without multer cha use karta pn apan lihuch shakto. so jithe jithe mala file uploading chi need asel tithe tithe mi multer middleware la inject karnar. for example, register kartana garaj padel so tithe yala inject karaych login madhe need nasel so tithe nahi inject karaych asa. so he utility sarkha nasnar but as a middlware apan yala saglikade use karu shakto. so middleware folder madhe multer.middleware.js file create keli so chala tyat.
+
+*/
